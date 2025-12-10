35c1664e444095fb253509cc0477b1dd1c435a6c
Andrii Fil <root.fi36@gmail.com>
2025-12-10 23:12:03 +0200

    files remove start _ gif

--- links.nim
+++ links.nim
@@ -5,19 +5,19 @@ import os
 proc links(path: string) =
   let dateText = readFile(path)
 
-  let datePattern = re"""graphic\\ODYSSEY\\([A-Z0-9]+)\\en_US\\_img.jpg"""
+  let datePattern = re"""graphic\\RTS\\([A-Z0-9]+)\\en_GB\\_img.gif"""
 
   let formattedDate = replacef(
     dateText,
     datePattern,
-    "graphic\\ODYSSEY\\$1\\en_US\\img.jpg")
+    "graphic\\RTS\\$1\\en_GB\\img.gif")
 
   echo path
   writeFile(path, formattedDate)
 
 proc ren(path: string) =
   let (dir, name, ext) = splitFile(path)
-  moveFile(path, dir & "/img.jpg")
+  moveFile(path, dir & "/img.gif")
 
-for f in walkFiles("/home/gs/xe_manual/topix/service/procedure/*"):
-  links(f)
+for f in walkFiles("/home/gs/xe_manual/topix/service/graphic/RTS/*/en_GB/_img.gif"):
+  ren(f)
